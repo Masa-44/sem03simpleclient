d88c57259d4c7abeb924fe74cd9e118069461287
Masa-44 <masa_40@outlook.com>
2023-04-22 19:57:50 +0000

    litt bedre kode

--- tcp_client.go
+++ tcp_client.go
@@ -13,18 +13,15 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer conn.Close()
 
 	log.Println("os.Args[1] = ", os.Args[1])
 
-	dekryptertMelding := mycrypt.Dekrypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
-	log.Println("Kryptert melding: ", string(dekryptertMelding))
+	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
 
-	_, err = conn.Write([]byte(string(dekryptertMelding)))
+	_, err = conn.Write([]byte(string(kryptertMelding)))
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	buf := make([]byte, 1024)
 	n, err := conn.Read(buf)
 	if err != nil {
@@ -32,4 +29,7 @@ func main() {
 	}
 	response := string(buf[:n])
 	log.Printf("reply from proxy: %s", response)
+	dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)+4)
+	log.Println("Dekrypter melding: ", string(dekryptertMelding))
+
 }
